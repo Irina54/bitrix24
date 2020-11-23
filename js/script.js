2565e071c7b93c6f3de40dca52df01463dec4214
Irina54 <irishka-508@mail.ru>
2020-11-23 18:18:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//const { active } = require("browser-sync")
+
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger, .menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
